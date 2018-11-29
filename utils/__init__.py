03eb4bf0855c698b96395b4f630c90eed6ebdc1b
Insane Shadow <insaneshadowzaman@gmail.com>
2018-11-29 12:56:26 +0600

    Updated for python 3

--- utils/__init__.py
+++ utils/__init__.py
@@ -24,11 +24,16 @@ along with pyAvroPhonetic.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 def utf(text):
-    """Shortcut funnction for encoding given text with utf-8"""
-    try:
-        output = unicode(text, encoding='utf-8')
-    except UnicodeDecodeError:
-        output = text
-    except TypeError:
-        output = text
+    import sys
+    if sys.version_info[0] < 3:
+        """Shortcut funnction for encoding given text with utf-8"""
+        try:
+            output = unicode(text, encoding='utf-8')
+        except UnicodeDecodeError:
+            output = text
+        except TypeError:
+            output = text
+            raise Exception("Must be using Python 3")
+    else:
+        output = str(text)
     return output
